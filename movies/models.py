d0731b57a93dfe2568dd2cb0c35f332e282b1dd4
Rajesh-K-C <rajeshkc1111.rk@gmail.com>
2025-04-28 15:15:00 +0545

    feat(models): add models for Language, Genre, and Movie

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    class Meta:
+        db_table = 'languages'
+        verbose_name_plural = 'languages'
+        
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    
+    class Meta:
+        db_table = 'genres'
+        verbose_name_plural = 'genres'
+
+    def __str__(self):
+        return self.name
+    
+    @property
+    def total_movies(self):
+        return self.movies.count()
+    
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    total_views = models.PositiveIntegerField(default=0)
+    total_likes = models.PositiveIntegerField(default=0)
+    url = models.URLField(unique=True)
+    poster = models.ImageField(upload_to='posters/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    genres = models.ManyToManyField(Genre, related_name='movies')
+    language = models.ForeignKey(Language, related_name='movies', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'movies'
+        verbose_name_plural = 'movies'
+
+    def __str__(self):
+        return self.title
