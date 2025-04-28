7681b004c925afc7c47bfc898fa97bd4db111d27
Rajesh-K-C <rajeshkc1111.rk@gmail.com>
2025-04-28 22:17:42 +0545

    feat(models): add models for WatchHistory, View, Like, and MyList

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Language(models.Model):
     name = models.CharField(max_length=20, unique=True)
@@ -7,7 +8,6 @@ class Language(models.Model):
         db_table = 'languages'
         verbose_name_plural = 'languages'
         
-
     def __str__(self):
         return self.name
 
@@ -43,3 +43,59 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+class WatchHistory(models.Model):
+    user = models.ForeignKey(User, related_name="watch_history", on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, related_name='watch_history', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'watch_histories'
+        verbose_name_plural = 'watch_histories'
+
+    def __str__(self):
+        return f"{self.user} watched {self.movie} at {self.created_at}"
+ 
+class View(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="views", null=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="views")
+    user_ip = models.IPAddressField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "views"
+        verbose_name_plural = "views"
+        indexes = [
+            models.Index(fields=["movie", "created_at"]),
+        ]
+
+    def __str__(self):
+        return f"{self.user} watched {self.movie} from {self.user_ip}"
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='likes', null=True) 
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "likes"
+        verbose_name_plural = "likes"
+        unique_together = ("user","movie")
+        indexes = [
+            models.Index(fields=["movie", "created_at"])
+        ]
+
+    def __str__(self):
+        return f"{self.user} liked {self.movie} at {self.created_at}"
+
+class MyList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="my_lists")
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="my_lists")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "my_lists"
+        verbose_name_plural = "my_lists"
+
+    def __str__(self):
+        return f"{self.user} added {self.movie} at {self.created_at}"
