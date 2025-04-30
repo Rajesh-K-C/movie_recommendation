02d146550eb3bc591cd2f9de077cb6bb7cba354b
Rajesh-K-C <rajeshkc1111.rk@gmail.com>
2025-04-30 07:29:38 +0545

    feat(admin): add delete buttons in admin list for Genre, Language, and Movie models

--- movies/admin.py
+++ movies/admin.py
@@ -1,17 +1,33 @@
 from django.contrib import admin
+from django.urls import reverse
 from .models import Movie, Genre, Language
 from django.utils.html import format_html
 
 class GenreAdmin(admin.ModelAdmin):
-    list_display = ('name', 'total_movies')
-    search_fields = ('name',)
-    
-class LanguageAdmin(admin.ModelAdmin):
-    list_display = ('name', )
+    list_display = ('name', 'total_movies', 'delete_button')
     search_fields = ('name',)
 
+    def delete_button(self, obj):
+        delete_url = reverse(f'admin:{obj._meta.app_label}_{obj._meta.model_name}_delete', args=[obj.pk])
+        return format_html(
+            '<a class="button" href="{}">Delete</a>',
+            delete_url
+        )
+    delete_button.short_description = 'Delete'
+    
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'delete_button' )
+    search_fields = ('name',)
+    def delete_button(self, obj):
+        delete_url = reverse(f'admin:{obj._meta.app_label}_{obj._meta.model_name}_delete', args=[obj.pk])
+        return format_html(
+            '<a class="button" href="{}">Delete</a>',
+            delete_url
+        )
+    delete_button.short_description = 'Delete'
+
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'display_poster', 'language', 'genre_list', 'total_views', 'total_likes')
+    list_display = ('title', 'display_poster', 'language', 'genre_list', 'total_views', 'total_likes', 'delete_button')
     list_filter = ('language', 'genres')
     search_fields=('title', )
     exclude = ("total_views", "total_likes")
@@ -25,6 +41,14 @@ class MovieAdmin(admin.ModelAdmin):
     def genre_list(self, obj):
         return ", ".join([genre.name for genre in obj.genres.all()])
     genre_list.short_description = 'Genres'
+    
+    def delete_button(self, obj):
+        delete_url = reverse(f'admin:{obj._meta.app_label}_{obj._meta.model_name}_delete', args=[obj.pk])
+        return format_html(
+            '<a class="button" href="{}">Delete</a>',
+            delete_url
+        )
+    delete_button.short_description = 'Delete'
 
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Language, LanguageAdmin)
