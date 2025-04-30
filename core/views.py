60e940dbb4c1815f8bd7d8a843a8ac80a376b37f
Rajesh-K-C <rajeshkc1111.rk@gmail.com>
2025-04-30 12:45:02 +0545

    feat(ui): add movie_card_row component and design homepage

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from movies.models import Movie
 
 def home(request):
-    recent_movies = Movie.objects.prefetch_related("language", "genres").order_by("-created_at")
-    context = {}
+    recent_movies = Movie.objects.prefetch_related("language", "genres").order_by("-created_at")[:7]
+    context = {
+        "recent_movies": recent_movies
+    }
     return render(request, "core/home.html", context)
