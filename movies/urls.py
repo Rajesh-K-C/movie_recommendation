f6f63a2fa717b98c7f7c99b8b9a2954801dfcaf9
Rajesh-K-C <rajeshkc1111.rk@gmail.com>
2025-05-03 23:58:30 +0545

    feat(genre): add genre list page and movie list by genre page with urls, views, and templates

--- movies/urls.py
+++ movies/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("watch/<int:pk>/", views.WatchView.as_view(), name="watch"),
     path("popular/", views.PopularMoviesView.as_view(), name="popular_movies"),
+    path("genres/", views.MovieGenreView.as_view(), name="movie_genre_list"),
+    path("genre/<int:pk>/", views.MovieByGenreView.as_view(), name="movie_list_by_genre"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from movies.models import Movie
+from movies.models import Movie, Genre
 from movie_recommendation.utils import format_minutes
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -22,4 +22,27 @@ class PopularMoviesView(LoginRequiredMixin, generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "Popular Movies"
+        return context
+
+class MovieGenreView(LoginRequiredMixin, generic.ListView):
+    model = Genre
+    queryset = Genre.objects.all().order_by("name")
+    template_name = "movies/movie_genre_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["genre_list"] = [movie for movie in context["genre_list"] if movie.total_movies > 0]
+        return context
+
+class MovieByGenreView(LoginRequiredMixin, generic.ListView):
+    model = Movie
+    template_name = "movies/movie_list.html"
+
+    def get_queryset(self):
+        self.genre = Genre.objects.get(pk=self.kwargs['pk'])
+        return Movie.objects.filter(genres=self.genre).order_by("-pk")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = f"{self.genre.name} Movies"
         return context
